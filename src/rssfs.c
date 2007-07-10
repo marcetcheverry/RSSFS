69bae9994713d53cfa2efe0758ee86fdf27af4b8
Marc <santusmarc@gmail.com>
2007-07-10 03:03:56 +0000

    Switch to GPLv3

--- src/rssfs.c
+++ src/rssfs.c
@@ -1,12 +1,24 @@
 /*
- * rssfs
- *
- * Copyright 2007 Marc E.
+ * RSSFS - RSS in userspace.
  * http://www.jardinpresente.com.ar/wiki/index.php/RSSFS
+ *
+ * Copyright (C) 2007 Marc E. <santusmarc@users.sourceforge.net>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * FUSE module
  * $Id$
- *
  */
 
 #define FUSE_USE_VERSION  26 
